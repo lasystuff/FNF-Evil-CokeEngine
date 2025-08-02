2d782d88a434d9db33c4e5d13c371d601750397b
Sulkiez <soushimiyatani0210@gmail.com>
2025-08-03 08:01:41 +0900

    cleanup stuff

--- _extra/assets_rework_idea/levels/weekend1.lua
+++
@@ -1,8 +0,0 @@
-display_name = "Weekend 1"
-title = "Due Debits"
-
--- Insert Blazin to songs if you finished blazin
-songs = ["darnell", "lit-up", "2hot"]
-if not SaveData.data._score:get("story_week1") == nil then
-	songs:insert("blazin")
-end
